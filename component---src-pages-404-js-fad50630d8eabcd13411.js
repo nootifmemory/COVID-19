002e1793d5d79eedd96b23156ba2c000f7d2fc3d
nootifmemory <nootifhuhong@gmail.com>
2021-04-08 21:18:00 +0700

    Hello

---
+++ component---src-pages-404-js-fad50630d8eabcd13411.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("Kvkj");t.default=function(){return l.a.createElement(u.b,null,l.a.createElement(u.a,{type:"content",className:"text-center"},l.a.createElement("h1",null,"Page Not Found"),l.a.createElement("p",null,"You just hit a route that doesn't exist...")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-fad50630d8eabcd13411.js.map
